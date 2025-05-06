c1c196b287aab8c4ea28adaf4432b9bb0371c09b
Costas Stefanakis <cst22590@gmail.com>
2025-05-06 16:02:49 +0300

    updLink

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="w-full h-[100vh] bg-gray-800">
       <div className="mx-4 sm:mx-[40%] pt-[40vh] flex flex-col gap-6 font-[family-name:var(--font-geist-sans)] justify-center ">
-        <a href="https://rent-a-car-scheduler.vercel.app/demos/FcScheduler" className={linkButtonStyle} >FcScheduler demo</a>
+        <a href="https://rent-a-car-scheduler.vercel.app/demos/FcScheduler" className={linkButtonStyle} >FcScheduler demo</a> {/*for local development, link to http://localhost:3000*/}
         <a href="https://rent-a-car-scheduler.vercel.app/demos/SyncfScheduler" className={linkButtonStyle} > Syncfusion Resource Scheduler Demo</a>
       </div>
     </div>
