001a2290d8438f8db2c9c58441f6ce46f7dd7c68
Costas Stefanakis <cst22590@gmail.com>
2025-05-13 16:07:58 +0300

    fixed read from sup prod?

--- app/addVehicle/page.jsx
+++ app/addVehicle/page.jsx
@@ -7,12 +7,20 @@
 // export default AddCar;
 
 import { createClient } from "@supabase/supabase-js";
+const env = process.env.NODE_ENV;
+let supUrl, supK, supS;
+
+if (env == "development") {
+  supUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  supK = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  supS = process.env.NEXT_PUBLIC_SUPABASE_SCHN;
+} else {
+  supUrl = process.env.SUP_SUPABASE_URL;
+  supK = process.env.SERVICE_ROLE_KEY;
+  supS = process.env.SUP_SUPABASE_SCHN;
+}
 export default async function Instruments() {
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    { db: { schema: "racsch" } }
-  );
-  const vehicles = await supabase.from("vehicle").select();
-  return <pre>{JSON.stringify(vehicles, null, 2)}</pre>;
+  const supabase = createClient(supUrl, supK, { db: { schema: supS } });
+  const res = await supabase.from("vehicle").select();
+  return <pre>{JSON.stringify(res.data)}</pre>;
 }
